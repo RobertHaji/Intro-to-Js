885bed4f422e900e9740c689beda17757548fb3a
Robert Haji <roberthadji66@gmail.com>
2025-03-03 17:05:17 +0300

    adds intro.js

---
+++ intro.js
@@ -0,0 +1,6 @@
+// console allows us to print out values
+// Syntax- Rules associated with a programming language
+/* shift+alt+a shortcut for mutiple comments */
+/* This are multi-line comments*/
+console.log(2 + 2);
+console.log(2 + (3 - 5));

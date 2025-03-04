3315e71b7ea7c4639160b4f5fe4b9597d59c5b32
Robert Haji <roberthadji66@gmail.com>
2025-03-04 10:28:52 +0300

    adds variables

--- intro.js
+++ intro.js
@@ -4,3 +4,47 @@
 /* This are multi-line comments*/
 console.log(2 + 2);
 console.log(2 + (3 - 5));
+/* Evaluation
+1. Expression
+2. Ass
+3. Lookup
+Evaluation is the breakdown of expressions to constant expressions
+
+variables in js
+*/
+// 1. constant - doesnt need to be broken down
+("Jane Doe");
+// Assignment - we use operator (=)
+// Syntax - on the left, we have the variable name then on the right
+// we have the value
+// A variable is just a container to store values for later retrivial
+/* Rules when naming variables
+1. We should use camelcase naming convention
+2. Js is case sensitive
+3. Don't start variables with numbers, always start with a lower case
+4. Do not use reserved keywors
+5. As a rule of thumb use const or let to create variables, there is also the var keyword but we do not it
+*/
+name = ("Jane Doe");
+firstName = ("Robert")
+secondName = ("Haji")
+dateofBirth = ("1994")
+console.log(firstName)
+console.log(secondName)
+console.log(dateofBirth)
+
+console.log(name)
+num = 3 + 6
+console.log(num) 
+// let keyword
+// allos reassigning of values
+let age = 18
+console.log(age)
+// reassignment - giving a different value to the same variable
+age = 20
+console.log(20)
+var goat = (Messi)
+console.log(goat)
+
+// Var keyword allows redeclaring
+// const keyword value is immutable
